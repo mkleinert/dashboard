a625ecbe7684fa8a47aee94d52fbc7383dcae118
Matt Kleinert <mkleinert@smarterhq.com>
2016-10-25 09:28:30 -0400

    initial push

---
+++ shq_dashboard.py
@@ -0,0 +1,35 @@
+__author__ = 'mkleinert'
+
+
+class Dashboard:
+    def __init__(self, src_database, src_server, src_user, src_password):
+        """ add later
+         self.client_id = src_client_id
+        """
+        self.src_database = src_database
+        self.src_server = src_server
+        self.src_user = src_user
+        self.src_password = src_password
+        
+        return
+
+    def main(self):
+
+
+if __name__=='__main__':
+     """     def __init__(self, client, table_list_dict, s3_mailbox, src_host, src_database,
+                    src_port, src_user, src_password, sql_proc_automation_fname,
+                    create_table_fname,
+                    temp_cluster_label = 'eng', sleep_interval = 180, NumberOfNodes=30):
+
+     """
+     shq_dashboard = Dashboard(
+                         #Main inputs needed for running the code
+                         src_database = '',
+                         src_server= '',
+                         src_user = '',
+                         src_password = '',
+
+     )
+
+     shq_dashboard.main()
